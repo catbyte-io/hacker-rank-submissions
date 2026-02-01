fbf51f453314c3d9ffdf969df9806c8ec1199d0f
catbyte-io <catbyte.io@gmail.com>
2026-02-01 16:43:55 -0700

    Find the minimum sum by excluding the largest value of the array, and the maximum sum by excluding the smallest value of the array. Use i64 to prevent buffer overflow. Return if 5 values aren't given or if the values are outside the constraints.

--- rust/easy/mini_max_sum.rs
+++ rust/easy/mini_max_sum.rs
@@ -1,30 +1,30 @@
 use std::io::{self, BufRead};
 
 /*
- * Complete the 'miniMaxSum' function below.
+ * PASSED 
+ Complete the 'miniMaxSum' function below.
  *
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-fn miniMaxSum(arr: &[i32]) {
-    /* check if array is empty */
-    if arr.is_empty():
-        return None
-        
-    /* initialize value for min value with first item in array */
-    let find_min = sums[0];
-    /* initialize value for max value */
-    let find_max = sums[0];
-
-    let mut i = 0;
-    for s in sums {
-        if s < sums[i] {
-            find_min = s;
-        }
-        else if s > sums[i] {
-            find_max = s;
-        }
+fn mini_max_sum(arr: &[i64]) {
+    /* check if array is correct length */
+    if arr.len() != 5 {
+        return;
     }
+
+    if arr.iter().any(|&x| x < 1) || arr.iter().any(|&x| x > 10_i64.pow(9)) {
+        return;
+    }
+
+    // calculate sum of all array elements
+    let total_sum: i64 = arr.iter().sum();
+        
+    // find minumum and maximum sum values
+    let find_min = total_sum - arr.iter().max().unwrap(); // exclude the max element
+  
+    let find_max = total_sum - arr.iter().min().unwrap();  // exclude the minumum element
+
     
     println!("{} {}", find_min, find_max);
 }
@@ -33,11 +33,11 @@ fn main() {
     let stdin = io::stdin();
     let mut stdin_iterator = stdin.lock().lines();
 
-    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+    let arr: Vec<i64> = stdin_iterator.next().unwrap().unwrap()
         .trim_end()
         .split(' ')
-        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .filter_map(|s| s.parse::<i64>().ok())
         .collect();
 
-    miniMaxSum(&arr);
+    mini_max_sum(&arr);
 }
