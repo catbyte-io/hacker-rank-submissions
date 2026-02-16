1047ccb36cc169e1480ef806a32f96a71f3b5c48
catbyte-io <catbyte.io@gmail.com>
2026-02-15 19:25:52 -0700

    Count the apples and oranges that fell within range

--- rust/easy/apples_oranges.rs
+++ rust/easy/apples_oranges.rs
@@ -18,13 +18,13 @@ fn count_apples_oranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges:
     let mut orange_positions = Vec::new();
 
     // calculate landing positions of apples
-    for apple in &apples {
+    for &apple in apples {
         let position = apple + a;  // adds the landing position plus the distance of the tree
         apple_positions.push(position);
     }
 
     // calculate landing positions of oranges
-    for orange in &oranges {
+    for &orange in oranges {
         let postion = orange + b;
         orange_positions.push(postion);
     }
@@ -32,6 +32,22 @@ fn count_apples_oranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges:
     // create counters for the apples and oranges that land within range
     let mut apple_count = 0;
     let mut orange_count = 0;
+
+    // calculate apples within range
+    for &apple in apple_positions {
+        if apple > s && apple < t {
+            apple_count += 1;
+        }
+    }
+    println!(apple_count);
+
+    // calculate oranges within range
+    for &orange in oranges {
+        if orange > s && orange < t {
+            orange_count += 1;
+        }
+    }
+    println!(orange_count);
 }
 
 fn main() {
