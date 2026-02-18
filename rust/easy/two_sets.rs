45749d0020e4da4d59ee88d4653f332030cc827a
catbyte-io <catbyte.io@gmail.com>
2026-02-18 13:21:51 -0700

    Implement get_total_x function

--- rust/easy/two_sets.rs
+++ rust/easy/two_sets.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
+use std::convert::TryInto;
 
 /*
  * Complete the 'getTotalX' function below.
@@ -12,7 +13,28 @@ use std::io::{self, BufRead, Write};
  */
 
 fn get_total_x(a: &[i32], b: &[i32]) -> i32 {
+    // Create vector to save the integers that meet the criteria
+    let mut between = Vec::new();
+    
+    // Counter for integers
+    let mut integer = a[a.len() - 1];
 
+    // Iterate through both arrays
+    while integer <= b[0] {
+        let mut count = 0; // Tracks the leftovers from mod division
+        for &i in a {
+            count += integer % i;
+        }
+        for &i in b {
+            count += i % integer;
+        }
+        // If there are no leftovers, the conditions are met
+        if count == 0 {
+            between.push(integer);
+        }
+        integer += 1;
+    }
+    return between.len().try_into().unwrap();  // Return the number of integers between the sets
 }
 
 fn main() {
@@ -26,9 +48,9 @@ fn main() {
         .map(|s| s.to_string())
         .collect();
 
-    let n = first_multiple_input[0].trim().parse::<i32>().unwrap();
+    let _n = first_multiple_input[0].trim().parse::<i32>().unwrap();
 
-    let m = first_multiple_input[1].trim().parse::<i32>().unwrap();
+    let _m = first_multiple_input[1].trim().parse::<i32>().unwrap();
 
     let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
         .trim_end()
